3057eff734b9f01cd7257dc957970436c14292ed
iAboud98 <aboudfialah98@gmail.com>
2025-10-09 14:54:51 +0300

    decrease the area of randomness in weights to be around 0

--- nn/layer.py
+++ nn/layer.py
@@ -6,7 +6,7 @@ class Layer:
 
     def __init__(self, neurons_num, features_num):
         self.biases = np.zeros((1,neurons_num))
-        self.weights = np.random.randn(neurons_num, features_num)
+        self.weights = 0.10 * np.random.randn(neurons_num, features_num)
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights.T) + self.biases
