3d99d1a93eea01a79a2e048b9c02ae8d2fcf1b2d
iAboud98 <aboudfialah98@gmail.com>
2025-10-09 14:26:40 +0300

    implement layer instructor and forward methods

---
+++ nn/layer.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+np.random.seed(0)
+
+class Layer:
+
+    def __init__(self, neurons_num, features_num):
+        self.biases = np.zeros((1,neurons_num))
+        self.weights = np.random.randn(neurons_num, features_num)
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights.T) + self.biases
