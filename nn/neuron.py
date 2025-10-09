c582826b6095830fecbacc32457ad65f99d244a9
iAboud98 <aboudfialah98@gmail.com>
2025-10-09 14:24:20 +0300

    implement Neuron functionality for learning only (not used in NN)

--- nn/neuron.py
+++ nn/neuron.py
@@ -1,12 +1,18 @@
+# this class is not used in NN, its only to show what happens inside a single neuron
+
+import numpy as np
+
+np.random.seed(0)
+
 class Neuron:
 
-    def __init__(self, inputs, weights, bias):
-        self.inputs = inputs
-        self.weights = weights
-        self.bias = bias
+    def __init__(self, features_num):
+        self.weights = np.random.randn(1, features_num)
+        self.bias = 0.0
+    
+    def forward(self, input):
+        self.output = np.dot(input, self.weights.T) + self.bias
 
-    def linear_output(self):
-        counter = 0
-        for _ in range(len(self.inputs)):
-            counter += self.inputs[_] * self.weights[_]
-        return counter + self.bias
+n1 = Neuron(3)
+n1.forward([[1,2,3],[2,2,4]])
+print(n1.output)
