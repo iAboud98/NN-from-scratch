d074a8ce4eaa5b59cee21ba1c8725e2649e3d11c
iAboud98 <aboudfialah98@gmail.com>
2025-10-10 20:52:42 +0300

    implement softmax function

--- nn/activation.py
+++ nn/activation.py
@@ -2,3 +2,7 @@ import numpy as np
 
 def ReLU(inputs):
     return np.maximum(0, inputs)
+
+def softmax(inputs):
+    exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims= True))
+    return exp_values / np.sum(exp_values, axis = 1, keepdims= True)
