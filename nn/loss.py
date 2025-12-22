b0827f683262ba1d6d5c5d1285d5375c0004623b
iAboud98 <aboudfialah98@gmail.com>
2025-12-23 00:10:31 +0200

    refactor loss functions to include clipping in cross-entropy calculation

--- nn/loss.py
+++ nn/loss.py
@@ -5,18 +5,26 @@ class BackPropagation:
     def __init__(self, predictions, true_vals):
         self.predictions = predictions
         self.true_vals = true_vals
-
+        self.encoded_true = None
 
     def one_hot(self, classes_number):
         self.true_vals = self.true_vals.flatten().astype(int)
         encodings = np.zeros((len(self.true_vals), classes_number))
         encodings[np.arange(len(self.true_vals)), self.true_vals] = 1
-
         return encodings
 
     def cross_entropy(self, encoded_true):
-        log_preds = np.log(self.predictions)
+        self.encoded_true = encoded_true
+
+        eps = 1e-12
+        clipped_preds = np.clip(self.predictions, eps, 1.0 - eps)
+
+        log_preds = np.log(clipped_preds)
         multiplied = encoded_true * log_preds
 
-        return -np.sum(multiplied, axis=1)
+        losses = -np.sum(multiplied, axis=1)
+        return losses.mean()
 
+    def d_softmax_cross_entropy(self):
+        N = self.predictions.shape[0]
+        return (self.predictions - self.encoded_true) / N
