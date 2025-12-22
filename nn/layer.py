ba2a0f19bf15984dcd0ffa71169c4ec41428a6f4
iAboud98 <aboudfialah98@gmail.com>
2025-12-23 00:12:06 +0200

    cache inputs and implement backward equations

--- nn/layer.py
+++ nn/layer.py
@@ -6,14 +6,49 @@ np.random.seed(0)
 class Layer:
 
     def __init__(self, neurons_num, features_num):
-        self.biases = np.zeros((1,neurons_num))
+        self.biases = np.zeros((1, neurons_num))
         self.weights = 0.10 * np.random.randn(neurons_num, features_num)
 
+        # caches (needed for backward)
+        self.inputs = None
+        self.pre_activation = None
+
+        # grads
+        self.dweights = None
+        self.dbiases = None
+        self.dinputs = None
+
+        # forward output
+        self.output = None
+
     def forward(self, inputs):
+        # cache inputs for backward
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weights.T) + self.biases
 
     def run_activation(self):
+        # cache pre-activation for ReLU backward
+        self.pre_activation = self.output
         self.output = ReLU(self.output)
 
     def run_softmax(self):
         self.output = softmax(self.output)
+
+    def backward(self, dvalues):
+        # dW: (neurons, features)
+        self.dweights = np.dot(dvalues.T, self.inputs)
+
+        # db: (1, neurons)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # dX: (N, features)
+        self.dinputs = np.dot(dvalues, self.weights)
+
+    def backward_activation(self, dvalues):
+        dZ = dvalues.copy()
+        dZ[self.pre_activation <= 0] = 0
+        return dZ
+
+    def apply_gradients(self, learning_rate):
+        self.weights -= learning_rate * self.dweights
+        self.biases  -= learning_rate * self.dbiases
