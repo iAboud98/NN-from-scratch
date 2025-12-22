61a16a1b9827aa94141883f54c8d1f802a14fe7f
iAboud98 <aboudfialah98@gmail.com>
2025-12-23 00:12:33 +0200

    make code readble

--- nn/activation.py
+++ nn/activation.py
@@ -4,5 +4,5 @@ def ReLU(inputs):
     return np.maximum(0, inputs)
 
 def softmax(inputs):
-    exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims= True))
-    return exp_values / np.sum(exp_values, axis = 1, keepdims= True)
+    exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+    return exp_values / np.sum(exp_values, axis=1, keepdims=True)
